4b0ee32f2b6482bbc446876b5bbbea18d9704f42
Chentao Yang <yangcht@gmail.com>
2024-08-07 23:47:14 +0200

    revert

--- relint/__init__.py
+++ relint/__init__.py
@@ -1,9 +1,6 @@
 """Write your own linting rules using regular expressions."""
 
-try:
-    from . import _version
-except ImportError:
-    _version = None
+from . import _version
 
-__version__ = getattr(_version, 'version', 'unknown')
-VERSION = getattr(_version, 'version_tuple', (0, 0, 0))
+__version__ = _version.version
+VERSION = _version.version_tuple
